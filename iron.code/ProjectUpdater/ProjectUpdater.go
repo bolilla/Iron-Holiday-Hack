2389b555060b7a104a97af3ed77ede2a2e7df73f
ROUX-B <borja.roux@gmail.com>
2013-12-29 17:38:35 +0100

    Ready but for the mocked up project information retrieval and pushing

--- iron.code/ProjectUpdater/ProjectUpdater.go
+++ iron.code/ProjectUpdater/ProjectUpdater.go
@@ -1,4 +1,4 @@
-//This worker pushes the projects to update from the scheduled queue to the to-do queue to be managed by "Sisyphus"
+//This worker reads the projects to push to appengine, reads the information from GitHub, pushes the information to appengine and re-enqueues the project to be updated in the next hour
 package main
 
 import (
@@ -6,11 +6,11 @@ import (
 	"github.com/iron-io/iron_go/mq"
 	"log"
 	"sync"
+	"time"
 )
 
 const (
-	queue_scheduled_projects = "scheduled_projects"
-	queue_to_do_projects     = "to_do"
+	queue_to_do_projects = "to_do"
 )
 
 func main() {
@@ -19,40 +19,82 @@ func main() {
 	cfg.ProjectId = "52bc58376d31d10005000031"
 	cfg.Token = "qefvrPEPKzEZEPNSZw7vz0VUh-s"
 
-	qFrom := mq.New(queue_scheduled_projects)
-	qFrom.Settings = cfg
-	info, err := qFrom.Info()
-	if err != nil {
-		log.Println("Queue info error ("+queue_scheduled_projects+"):", err)
-		return
-	}
-	log.Println("Queue \"", qFrom, "\":", info)
-
-	qTo := mq.New(queue_to_do_projects)
-	qTo.Settings = cfg
-	info, err = qTo.Info()
+	q := mq.New(queue_to_do_projects)
+	q.Settings = cfg
+	info, err := q.Info()
 	if err != nil {
 		log.Println("Queue info error ("+queue_to_do_projects+"):", err)
 		return
 	}
-	log.Println("Queue \"", qTo, "\":", info)
+	log.Println("Queue \"", q, "\":", info)
+	sisyphus(q)
+}
 
+//Copies the messages in qFrom to qTo and waits one minute before checking again
+func sisyphus(q *mq.Queue) {
+	for {
+		processQueue(q)
+		time.Sleep(time.Minute / 2)
+	}
+}
+
+//processes the messages in the queue
+func processQueue(q *mq.Queue) {
 	var wg sync.WaitGroup
-	for msg, err := qFrom.Get(); err == nil; msg, err = qFrom.Get() {
+	log.Println("Checking queue...")
+	for msg, err := q.Get(); err == nil; msg, err = q.Get() {
 		wg.Add(1)
-		go push(qTo, msg, &wg)
+		go manageProject(q, msg, &wg)
 	}
 	log.Println("Waiting for the end of the pushing")
 	wg.Wait()
-	log.Println("All messages pushed from \"" + queue_scheduled_projects + "\" to \"" + queue_to_do_projects + "\"")
+	log.Println("All project information updated.")
 }
 
-func push(q *mq.Queue, msg *mq.Message, wg *sync.WaitGroup) {
+//Manages the information in a project
+func manageProject(q *mq.Queue, msg *mq.Message, wg *sync.WaitGroup) {
 	defer wg.Done()
-	id, err := q.PushMessage(msg)
-	if err == nil {
-		log.Println("Message \""+id+"\" pushed: ", msg)
+	projectInformation, err := getProjectInformation(msg)
+	if err != nil {
+		log.Println("Error getting information of project:", msg)
 	} else {
-		log.Println("Error pushing \"", msg, "\":", err)
+		err = pushProjectInformation(projectInformation)
+		if err != nil {
+			log.Println("Error pushing information of project", msg)
+		} else {
+			msg.Delete()
+			msg.Delay = 3600
+			_, err = q.PushMessage(msg)
+		}
 	}
+	//id, err := qTo.PushMessage(msg)
+	//if err == nil {
+	//	log.Println("Message \""+id+"\" pushed: ", msg)
+	//	msg.Delay = 3600
+	//	_, err = qFrom.PushMessage(msg)
+	//	if err != nil {
+	//		log.Println("Error copying message \""+id+"\": ", err)
+	//	} else {
+	//		msg.Delete()
+	//		log.Println("Message \"" + id + "\" re-enqueued")
+	//	}
+	//} else {
+	//	log.Println("Error pushing \"", msg, "\":", err)
+	//}
+}
+
+//Gets the information from GitHub
+func getProjectInformation(msg *mq.Message) (result prjInfo, err error) {
+	log.Println("Mocking project information retrieval", msg.Body)
+	return
+}
+
+//Pushes the information to appengine
+func pushProjectInformation(projectInformation prjInfo) error {
+	log.Println("Mocking project information pushing")
+	return nil
+}
+
+//Contains all the information elements of a project
+type prjInfo struct {
 }
